baf54229187d9575ba22077b2bfaeb953cdcb12a
Gary Siu <Gary@untypeable.com>
2015-12-11 12:11:38 +0000

    Stubbed out routes

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
 require 'sinatra/base'
 class MyApp < Sinatra::Base
   
-  get '/' do
-    "Hello World"
+  # INDEX
+  get '/movies/?' do
+    'Index of movies'
+  end
+
+  # CREATE
+  post '/movies/?' do
+    'You made a movie'
+  end
+
+  # SHOW
+  get '/movies/:id/?' do
+    'Showing 1 movie'
+  end
+
+  # DELETE
+  delete '/movies/:id/?' do
+    'You deleted a movie'
+  end
+
+  # UPDATE
+  patch '/movies/:id/?' do
+    'You updated a movie'
   end
 
   # start the server if ruby file executed directly
